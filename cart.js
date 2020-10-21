590f7c5b0dfcf9bd5153d7d67e4f44086e37edb4
Pari <pari.ramamoorthy@gmail.com>
2020-10-20 23:36:46 -0400

    Adding del,cart Icons & fns

---
+++ cart.js
@@ -0,0 +1,14 @@
+let itemQty = [];
+let totalQty = [];
+
+function delItems(id){   
+     document.getElementById(id).style.color="blue";
+}
+
+function addItems (id) {    
+   let targetCart = document.getElementsByClassName("cart")[id];
+   targetCart.style.display="none";
+   let targertRibbon= document.getElementsByClassName("ribbon")[id];
+   targertRibbon.style.display="block";
+   document.get
+}
